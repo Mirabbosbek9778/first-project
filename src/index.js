d06fb5d65ffdc5cde012a355103f4a185d0137aa
Mirabbosbek <mirabbosabduvohidov1@gmail.com>
2023-04-10 05:15:27 +0500

    first project card

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import Carusel from './carusel';
+
+ReactDOM.render(
+  // <App />,
+  <Carusel/>,
+  document.getElementById('root')
+);
