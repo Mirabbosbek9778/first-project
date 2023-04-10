200547e20b5537f27ec2b361a4c6fc73fe4756f5
Mirabbosbek <mirabbosabduvohidov1@gmail.com>
2023-04-10 08:55:54 +0500

    card

--- src/card.jsx
+++ src/card.jsx
@@ -1,58 +1,65 @@
 import React from "react";
 import "./caruselStyle.css";
+import Ellip from './assets/icon/Ellipse 11.svg'
+import Sofa from './assets/icon/sofa.svg'
+import Vanna from './assets/icon/vanna.svg'
+import Car from './assets/icon/car.svg'
+import Ter from './assets/icon/triangle.svg'
+import Love from './assets/icon/love 1.svg'
+import Line from './assets/icon/1.svg'
 
 class Card extends React.Component {
   render() {
     return (
       <div className="container">
-        <img
-          src="https://s3-alpha-sig.figma.com/img/d6c4/4e0e/047ff22cd0b54d65a00d57427410b161?Expires=1681689600&Signature=MU7ThV4q~FqS9atZPqJEiW9cNQTHZG6dbujNp9wdF9CBgkxKyFinhzzP6M568l8GT6R25pRVsxNpfKUsz3cMlZQ9ifG8EFISDjbw1lCubcjekP3lS-tejsDVUcbrEKiX14Yk1koIMzscZEnhe5aXC4c9sH8~iFM9XjiCJK~JdlsuE0iSufSThmCClgpRaTNVAEV0lu9xJ01W0FnapPdvDazkhD6xacXerqgrTFenOtl2PWQvoEKymG1uX0bf0EvdBfVazB0S-NpEBjns1nivr6eab2f0yBDZNBNOqjR2-C89XgZp703SpczBcSQG~EN4nwdsHwqzfbANe0oKpW~LMw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+        <img 
+          src={this.props.src}
           alt=""
         />
         <img
           className="cyrcle"
-          src="https://s3-alpha-sig.figma.com/img/53cd/8e33/dcfee82afbf5810c3c017a0704b153c0?Expires=1681689600&Signature=qvEKyC4ukWcAR0vsioUUOuBJQKCR7tefR5hlAOIJDRyeFrf04qoJiqPO3FnXScxkTyIRAisWVmiwEl4fEog3~UmBw43ITr-YCdNt4Vhw6yKlB-ZCDiLDd-qDbdHr~s0okXILv0Z~nX9ZUgZNMjOkYKacoVn-lvWL~n8PRLuPp~WGKE8Zs-Hu5lZZyIT3W2rfYQqN1uFAScME~I3-Es~ZTQ4G0nz7HpSfx~9A4cYxXlQ9Fl1h9q5doWe9x4OIilM-pM4tsrhqbEncY9uL2cVjQoxxbDrBXGtTu1PQlO9fJweikyng2jhodN2N-hifQAXkf2m06ZEoCbpzEkhl~btoRA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          src={Ellip}
           alt=""
         />
         <button className="but">Featured</button>
         <button className="but2">For Sale</button>
         <p>
-          <b>New Apartment Nice Wiew</b>
+          <b className="new">New Apartment Nice Wiew</b>
         </p>
         <p>Quincy St, Brooklyn, NY, USA</p>
         <div className="icon">
           <div className="wrap">
             <img
               className="icons"
-              src="https://cdn-icons-png.flaticon.com/512/333/333493.png"
+              src={Sofa}
               alt=""
             />
-            <p>4beds</p>
+            <p>4 Beds</p>
           </div>
           <div className="wrap">
 
           <img
             className="icons"
-            src="https://cdn-icons-png.flaticon.com/512/333/333493.png"
+            src={Vanna}
             alt=""
             />
-            <p>5 baths</p>
+            <p>5 Baths</p>
             </div>
             <div className="wrap">
 
           <img
             className="icons"
-            src="https://cdn-icons-png.flaticon.com/512/333/333493.png"
+            src={Car}
             alt=""
             />
-            <p>1 garage</p>
+            <p>1 Garage</p>
             </div>
             <div className="wrap">
 
 
           <img
             className="icons"
-            src="https://cdn-icons-png.flaticon.com/512/333/333493.png"
+            src={Ter}
             alt=""
             />
             <p>120 Sq Ft</p>
@@ -66,8 +73,8 @@ class Card extends React.Component {
             <b className="count">$7,500/mo</b>
             </div>
             <div className="ims">
-            <img className="like" src="https://as2.ftcdn.net/v2/jpg/02/13/71/41/500_F_213714173_zx3pF8cy6yFFghIQ3NldmGpnx1pmPTBF.jpg" alt="" />
-            <img className="like" src="https://as2.ftcdn.net/v2/jpg/02/13/71/41/500_F_213714173_zx3pF8cy6yFFghIQ3NldmGpnx1pmPTBF.jpg" alt="" />
+            <img className="like" src={Line} alt="" />
+            <img className="like" src={Love} alt="" />
             </div>
         </div>
       </div>
--- src/carusel.jsx
+++ src/carusel.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import Card from "./card";
 import "./index.css";
+import Tree from "./assets/imgs/yester.png";
 
 class Carusel extends React.Component {
   render() {
     return (
       <div className="wrapper">
         <Card
-          title="hous"
+          title="tit"
           src="https://s3-alpha-sig.figma.com/img/d6c4/4e0e/047ff22cd0b54d65a00d57427410b161?Expires=1682294400&Signature=oPj4nCmL3f6QbxijQemH55LhF8qHBja-xgN9sAN6LRCyy5gKMEEX8kCkDOQDeExFswm9Mx7eie5Y2tc-u-4~xDBLn8V0mPhVj5XqSQ2QP4UlBXirfg7mxN9RZ0kI6IOhO9cnFKvd3xCaAhRl2R2u3Tlcx09CTcnj05k7oh8HnWUWkWvOY7i93hJXj0h49VLFXa0ZUMv2c-19ASLX2NL7fPV3SsT9017ePM7u-sSINljwB3mioh3ri5m8ELjiCfqWnO2o-2Yzqxdp~OdkhkOF8BSDEyawPZcIfRTXgTokVvyvXIKGwg80MuEhCA~AZH2SWM09tsSl~-8NxZBzwh4yIg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
         />
         <Card
-          title="tit"
+          title="hous"
           src="https://s3-alpha-sig.figma.com/img/348a/9872/65add135c66e5d356aae0e7a44733748?Expires=1682294400&Signature=hR8-WvYoc16II7jHr1RyMBolPPlsnuCdxrh0UjBBOJshDjoredUEu1VDhHlWmthHJygCqvzvYqCBx50XVYmi0MdWB4DuNi8-GKLXQxi3RGKGTPOftgqelgHmm4I4ZxFxucWcLiTv6Ld0dqbojJJ8rwF-MZ1VLGn1g92vJDfrXG5S8pdml7T7UgQhEKX-Rym2Va9EelxQiJ-JDdq6imDbhrEzl9st3a3phWFMJ99pQGqG9hPbDEFlCJLn-4kP-z0ZnRBjPbl5NaosMX-xPo1KLM~ovapwwK4gMsLaPZDNYLT8A9f1~mamqvSu1gGyULWRxeXei0iPWhRm2CmNyNN5kQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
         />
-        <Card
-          title="apple"
-          src="https://s3-alpha-sig.figma.com/img/1334/8138/c3ae1f3fbf2095160c9034f999741525?Expires=1682294400&Signature=KPkGSxHOBu7vcT6JBbxXXnb8BdyHq9DpXGLWRo3Pg81wtcIdKW6vR42o7UC4tCo29f-oVbHX~LHXF~LcDYhwJkuc1kHEUJ99atq-1HI4fZ5qPLOpGgNIMVWXhBqAN999q2J00BP7kBXpq2n6wmwNTDS2fuFGl~yAa0FncStO8Tda4CCDEbd8pTdrXzGEHjb2XZ0Gf8nyhYdyuUrB0MD8N~4B-ia1fS~66ORV3DPJDQkvKt-Fj2ZAb19sT8PRn4EAmiAUsW05LkiCH4Q0Gj9G1H0OXQhS0BGvdcK5ulx0bqreGutQdm5dXFXtQpD8iBp~EBtNH2zFSE17-Ook1r6yIw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        />
-        
+        <Card title="apple" src={Tree} />
       </div>
     );
   }
